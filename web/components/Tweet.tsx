507b347340683243b601fe86d3b26bfadee3f538
Angélica Shigematsu <78798754+angelica-shigematsu@users.noreply.github.com>
2022-11-01 16:57:57 -0300

    Added class 1 back-end

---
+++ web/components/Tweet.tsx
@@ -0,0 +1,9 @@
+export function Tweet(props) {
+  return (
+    <div>
+      <h1>Tweet</h1>
+      <p>{props.text}</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero, perferendis aspernatur? Ipsam tempora aliquid iste nulla nisi placeat minima, iusto officia, exercitationem voluptatibus nemo non id distinctio! Voluptatibus, ipsa totam?</p>
+    </div>
+  )
+}
