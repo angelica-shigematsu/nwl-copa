5cc13e13947b5a7a4723d0b5f02a1b878ed7c420
Angélica Shigematsu <78798754+angelica-shigematsu@users.noreply.github.com>
2022-11-02 21:33:20 -0300

    Class 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,55 @@
 import Fastify from 'fastify'
+import {PrismaClient} from '@prisma/client'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
-
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 const prisma = new PrismaClient({
   log: ['query'],
 })
+
 async function bootstrap() {
   const fastify = Fastify({
     logger: true, //-> retorna alguma erro na aplicação
   })
 
-  await fastify.register(cors, {//mexanismo de segurança
-    origin: true//qualquer os dados do backend acessa a aplicação
+  await fastify.register(cors, {
+    origin: true,
+  })
+  
+  fastify.get('/pools/count', async() => {
+    const pools = await prisma.pool.count()
+    return { pools }
   })
 
-  fastify.get('/pools/count', async() => {
+  fastify.post('/pools', async(request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    })
+    
+    try{
+      const { title } = createPoolBody.parse(request.body)
+      
+      const generate = new ShortUniqueId({ length: 6 })
+      
+      const code = String(generate()).toLocaleUpperCase()
+      await prisma.pool.create({
+        data: {
+          title,
+          code 
+        }
+      })
+      
+      return reply.status(201).send({ code })
+    }catch(err){
+      return reply.status(401).send('Título nulo')
+    }    
+  })
+  fastify.get('/guesses/count', async() => {
+    const pools = await prisma.pool.count()
+    return { pools }
+  })
+
+  fastify.get('/users/count', async() => {
     const pools = await prisma.pool.count()
     return { pools }
   })
